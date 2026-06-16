dc761e4cb0c89dffc9969976bc26891273f84c87
yanzi10 <vanduy.311005@gmail.com>
2026-06-16 16:49:18 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,33 @@
+window.addEventListener("scroll", () => {
 
+    const cards =
+    document.querySelectorAll(
+    ".card,.project-card"
+    );
+
+    cards.forEach(card => {
+
+        const top =
+        card.getBoundingClientRect().top;
+
+        if(top < window.innerHeight - 100){
+
+            card.style.opacity = "1";
+            card.style.transform =
+            "translateY(0)";
+        }
+    });
+});
+
+document
+.querySelectorAll(
+".card,.project-card"
+)
+.forEach(item => {
+
+    item.style.opacity = "0";
+    item.style.transform =
+    "translateY(40px)";
+    item.style.transition =
+    "all 0.6s ease";
+});
